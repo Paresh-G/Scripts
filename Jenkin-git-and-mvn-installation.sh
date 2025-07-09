12590885ddf44454c705dab112b0fd61c13f0d39
Paresh-G <pareshgoenka8@gmail.com>
2025-07-09 18:49:18 +0530

    Update Jenkin-git-and-mvn-installation.sh

--- Jenkin-git-and-mvn-installation.sh
+++ Jenkin-git-and-mvn-installation.sh
@@ -25,5 +25,10 @@ sed -i 's|PATH=$PATH:$HOME/.local/bin:$HOME/bin|PATH=$PATH:$HOME/.local/bin:$HOM
 
 #Refreshing path
 source .bash_profile
+source .bash_profile
+source .bash_profile
 
+wget -O /home/ec2-user/Server-Health-checkup.sh https://raw.githubusercontent.com/Paresh-G/Scripts/master/Server-Health-checkup.sh
+
+sudo sh /home/ec2-user/Server-Health-checkup.sh
 
